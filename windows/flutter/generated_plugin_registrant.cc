26732a3dfaebf97c31b36f40a5cc8a7024ef033d
Nasri <nasri@esmartech.my>
2026-07-06 10:13:00 +0800

    Add : package : include hid tools

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <hid_tool/hid_tool_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  HidToolPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("HidToolPluginCApi"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  hid_tool
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
